72673c41c29ff67c9b7a107581c5b3c21b10f90f
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-27 15:33:19 +0200

    creazione header, inseriti in navlink solo fino a news e inserito a mano shop così da poter utilizzare di nuovo navlink per il footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
+import NavLink from "./NavLink";
 function Header(){
-    // const h1 = <h1>Titolo</h1>;
-    // return h1; stessa cosa di quello sotto
     return <header>
-        <h1>Titolo</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta distinctio voluptatum sint, officia quae odit ipsa atque quam quibusdam adipisci quas nam quidem, cupiditate vel placeat! Placeat perspiciatis eaque recusandae?</p>
+        <img src="/imgs/dc-logo.png" alt="Logo DC" />
+        <NavLink/>
+        <a href="SHOP">SHOP</a>
     </header>; 
 }
 
---
+++ src/components/NavLink.jsx
@@ -0,0 +1,15 @@
+function NavLink(){
+    return <>
+    <a href="characters">CHARACTERS</a>
+    <a href="COMICS">COMICS</a>
+    <a href="MOVIES">MOVIES</a>
+    <a href="TV">TV</a>
+    <a href="GAMES">GAMES</a>
+    <a href="COLLECTIBLES">COLLECTIBLES</a>
+    <a href="VIDEOS">VIDEOS</a>
+    <a href="FANS">FANS</a>
+    <a href="NEWS">NEWS</a>
+    </>;
+}
+
+export default NavLink;
