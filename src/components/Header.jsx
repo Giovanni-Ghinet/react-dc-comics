9d02e02ea72600e64bd69bde55f1a9b082bf042b
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-27 14:57:11 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    // const h1 = <h1>Titolo</h1>;
+    // return h1; stessa cosa di quello sotto
+    return <header>
+        <h1>Titolo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta distinctio voluptatum sint, officia quae odit ipsa atque quam quibusdam adipisci quas nam quidem, cupiditate vel placeat! Placeat perspiciatis eaque recusandae?</p>
+    </header>; 
+}
+
+export default Header;
